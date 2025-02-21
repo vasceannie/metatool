7671e5e975384e43d47678659662005983b85e8c
James Zhang <zhjch05@live.com>
2025-02-22 01:26:38 +0800

    0.1.1

--- src/client.ts
+++ src/client.ts
@@ -19,7 +19,7 @@ export const createMetaMcpClient = (
   const client = new Client(
     {
       name: "MetaMCP",
-      version: "0.1.0",
+      version: "0.1.1",
     },
     {
       capabilities: {
--- src/mcp-proxy.ts
+++ src/mcp-proxy.ts
@@ -34,7 +34,7 @@ export const createServer = async () => {
   const server = new Server(
     {
       name: "MetaMCP",
-      version: "0.1.0",
+      version: "0.1.1",
     },
     {
       capabilities: {
